c32cfef4ade1cb3fb5cb9d90b8ed89e732c8b86e
Jin <132893328+Jin1370@users.noreply.github.com>
2025-08-27 23:45:28 +0900

    Update code.js

--- 기타/이상한 문자 만들기/code.js
+++ 기타/이상한 문자 만들기/code.js
@@ -1,16 +1,34 @@
+// 시도
+// function solution(s) {
+//     s = s.split("");
+//     for(let i=0;i<s.length;i+=2){
+//         if(s[i]!==" "){
+//             s[i] = s[i].toUpperCase();
+//         }
+//         else{
+//             i--;
+//             continue;
+//         }
+//         if(s[i+1]){
+//             s[i+1] = s[i+1].toLowerCase();
+//         }
+//     }
+//     return s.join("");
+// }
+
+// 개선
 function solution(s) {
-    s = s.split("");
-    for(let i=0;i<s.length;i+=2){
-        if(s[i]!==" "){
-            s[i] = s[i].toUpperCase();
-        }
-        else{
-            i--;
-            continue;
-        }
-        if(s[i+1]){
-            s[i+1] = s[i+1].toLowerCase();
-        }
-    }
-    return s.join("");
+    return s
+        .split(" ")
+        .map(word =>
+            word
+                .split("")
+                .map((ch, idx) =>
+                    idx % 2 === 0 
+                        ? ch.toUpperCase()
+                        : ch.toLowerCase()
+                )
+                .join("")
+        )
+        .join(" ");
 }
