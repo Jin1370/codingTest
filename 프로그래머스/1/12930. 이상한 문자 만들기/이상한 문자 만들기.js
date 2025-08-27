c1d5f760eeb30dde99016d72a0dd6c6b182cf3c0
Jin <132893328+Jin1370@users.noreply.github.com>
2025-08-27 23:44:43 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.27 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -0,0 +1,15 @@
+function solution(s) {
+    return s
+        .split(" ")
+        .map(word =>
+            word
+                .split("")
+                .map((ch, idx) =>
+                    idx % 2 === 0 
+                        ? ch.toUpperCase()
+                        : ch.toLowerCase()
+                )
+                .join("")
+        )
+        .join(" ");
+}
