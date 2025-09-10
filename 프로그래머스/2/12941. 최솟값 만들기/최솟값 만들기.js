19c328f1924d1a48e06d3f8f3ebc527d445d09f2
Jin <132893328+Jin1370@users.noreply.github.com>
2025-09-10 16:51:12 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.85 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -2,7 +2,5 @@ function solution(A,B){
     let sum=0;
     A.sort((a,b)=>a-b);
     B.sort((a,b)=>b-a);
-    return A.reduce((acc, cur, idx) => {
-        return sum += cur * B[idx]
-        }, 0)
+    return A.reduce((acc, cur, idx) => sum += cur * B[idx], 0)
 }
