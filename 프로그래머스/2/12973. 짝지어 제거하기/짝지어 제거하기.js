0e609ec2575570bf0edb656a52d8ac4cacad193e
Jin <132893328+Jin1370@users.noreply.github.com>
2025-09-23 08:42:28 +0900

    [level 2] Title: 짝지어 제거하기, Time: 46.72 ms, Memory: 43.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,14 @@
+function solution(s)
+{
+    if(s.length % 2 !== 0) return 0
+    let stack = [];
+    for(let i=0;i<s.length;i++){
+        if(stack[stack.length-1]===s[i]){
+            stack.pop();
+        }
+        else{
+            stack.push(s[i])
+        }
+    };
+    return stack.length>0 ? 0 : 1;
+}
