3904b4a3b98bfc83d762086c120e606acf438bf1
Jin <132893328+Jin1370@users.noreply.github.com>
2025-10-24 23:44:43 +0900

    [level 2] Title: 예상 대진표, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,14 +1,9 @@
 function solution(n,a,b)
 {
-    let N = n;
-    let twoSquared = 0;
-    while(N!==1){
-        N=N/2;
-        twoSquared++;
-    }
-    
+    let twoSquared = Math.log2(n);
     let start = 1;
     let end = n;
+    
     for(let i=0;i<twoSquared;i++){
         let middle = start+(end-start+1)/2;
         if(a<middle && b<middle){
