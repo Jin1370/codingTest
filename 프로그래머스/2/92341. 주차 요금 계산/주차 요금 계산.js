ae7615f9531fd9c09270443aba6504f2f662a4ab
Jin <132893328+Jin1370@users.noreply.github.com>
2025-12-19 22:30:15 +0900

    [level 2] Title: 주차 요금 계산, Time: 3.47 ms, Memory: 34.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.js
@@ -0,0 +1,36 @@
+function solution(fees, records) {
+    const basicTime = fees[0];
+    const basicFare = fees[1];
+    const unitTime = fees[2];
+    const unitFare = fees[3];
+    const myMap = new Map();
+    let answer = [];
+    
+    records.forEach(record => {
+        let [time, carNum, inOut] = record.split(" ");
+        time = time.split(":").map(x => Number(x));
+        time = time[0] * 60 + time[1];
+        if(myMap.has(carNum)){
+            myMap.get(carNum).push(time);
+        } else myMap.set(carNum, [time]);
+    })
+    
+    for(let carNum of myMap.keys()){
+        let times = myMap.get(carNum);
+        if(times.length%2!==0) times.push(1439); //입차로 끝난 경우 23:59 출차로 기록
+        
+        let totalTime = 0;
+        for(let i=0;i<times.length;i+=2){
+            totalTime+=times[i+1]-times[i]; //(출차시간-입차시간)의 합산
+        }
+        
+        let fee = totalTime > basicTime ? 
+            basicFare + Math.ceil((totalTime-basicTime)/unitTime)*unitFare : 
+            basicFare;
+        myMap.set(carNum, fee);
+    }
+    
+    const sortedMap = new Map([...myMap.entries()].sort((a,b)=>a[0]-b[0]));
+    for(let value of sortedMap.values()) answer.push(value);
+    return answer;
+}
