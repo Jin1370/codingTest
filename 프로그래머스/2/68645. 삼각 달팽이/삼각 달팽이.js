2d74440e861607e6cd891c905b11dca5ea392102
Jin <132893328+Jin1370@users.noreply.github.com>
2026-01-12 23:40:39 +0900

    [level 2] Title: 삼각 달팽이, Time: 51.82 ms, Memory: 99.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.js
@@ -0,0 +1,33 @@
+function solution(n) {
+    let row = -1;
+    let col = 0;
+    let num = 1;
+    let repeat = n;
+    const arr = Array.from({length:n}, () => []);
+    
+    while(repeat>0){
+        for(let j=0;j<repeat;j++){
+            row++;
+            arr[row][col] = num;
+            num++;
+        }
+        repeat--;
+        for(let j=0;j<repeat;j++){
+            col++;
+            arr[row][col] = num;
+            num++;
+        }
+        repeat--;
+        for(let j=0;j<repeat;j++){
+            row--;
+            col--;
+            arr[row][col] = num;
+            num++;
+        }
+        repeat--;
+    }
+    const answer = [];
+    arr.forEach(x => answer.push(...x));
+    
+    return(answer);
+}
