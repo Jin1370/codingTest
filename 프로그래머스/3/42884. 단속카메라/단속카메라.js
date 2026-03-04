77fd19b4f7f37419a06dd2b1861a74624d371f30
Jin <132893328+Jin1370@users.noreply.github.com>
2026-03-04 11:12:28 +0900

    [level 3] Title: 단속카메라, Time: 19.66 ms, Memory: 35.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.js
@@ -0,0 +1,14 @@
+function solution(routes) {
+    let cameraPoint = -30000;
+    let cameraCount = 0;
+    routes.sort((a,b)=>a[1]-b[1]);
+    
+    for(let route of routes){
+        if(route[0] > cameraPoint){
+            cameraPoint = route[1];
+            cameraCount++;
+        }
+    }
+    
+    return cameraCount;
+}
