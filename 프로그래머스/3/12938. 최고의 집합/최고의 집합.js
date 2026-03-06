d50b85bec8b446ab527262c541683b928a485185
Jin <132893328+Jin1370@users.noreply.github.com>
2026-03-06 21:47:19 +0900

    [level 3] Title: 최고의 집합, Time: 0.53 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.js
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.js
@@ -1,11 +1,10 @@
 function solution(n, s) {
     const quotient = Math.floor(s/n);
-    const rest = s%n;
+    const remain = s%n;
     
     if(quotient<1) return [-1];
-    const answer = Array(n-rest).fill(quotient);
-    for(let i=0;i<rest;i++) answer.push(quotient+1);
+    const answer = Array(n-remain).fill(quotient);
+    for(let i=0;i<remain;i++) answer.push(quotient+1);
     
     return answer;
-    
 }
